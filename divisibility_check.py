69bfda297d1b26226f91547d87e6c0ca5f62aa9c
parkerS123 <23pschwantes@athenian.org>
2020-09-09 22:35:57 +0000

    divisible final

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,3 +1,13 @@
-# Collaborators: 
+# Collaborators: mom
 #
 
+for x in range(2,51):
+   
+    if x % 3 == 0:
+        print(x)
+        print("is divisible by 3")
+    else: 
+        print(x)
+        print(" is not divisible by 3")
+
+    
