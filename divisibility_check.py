137502f12966712b5105aac54d4e99103b8e3e10
parkerS123 <23pschwantes@athenian.org>
2020-09-12 19:02:27 +0000

    made it on same line

--- divisibility_check.py
+++ divisibility_check.py
@@ -4,10 +4,8 @@
 for x in range(2,51):
    
     if x % 3 == 0:
-        print(x)
-        print("is divisible by 3")
+        print(str(x) + " is divisible by 3")
     else: 
-        print(x)
-        print(" is not divisible by 3")
+        print(str(x) + " is not divisible by 3")
 
     
